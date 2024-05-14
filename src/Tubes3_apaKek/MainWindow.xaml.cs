f65236547956603e8667737f2fc188c314715c59
AlbertChoe <albert.choe73@gmail.com>
2024-05-14 23:22:10 +0700

    initializing WPF

---
+++ src/Tubes3_apaKek/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Tubes3_apaKek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnInsertImage(object sender, RoutedEventArgs e)
+        {
+            // Code to insert fingerprint image
+        }
+
+        private void OnSearch(object sender, RoutedEventArgs e)
+        {
+            // Code to perform search
+        }
+    }
+}
